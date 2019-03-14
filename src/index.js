077054ce6423c705cc38cf24213045b9fc9383a7
simona trotti <simona.trotti90@gmail.com>
2019-03-14 22:08:35 +0100

    sass aggiunto

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
+import './style/css/main.css';
 import * as serviceWorker from './serviceWorker';
 
 
