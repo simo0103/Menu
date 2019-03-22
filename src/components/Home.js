d7a7863fd6f010b9085f4b752b680c52860dbf64
Simona Trotti <simona.trotti.external@ynap.com>
2019-03-22 17:16:29 +0100

    slick

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Slider from './utils/Slider';
 
 class Home extends Component {
   render() {
 
     return (
       <div className="Home">
+      <Slider></Slider>
         <div>
-          <img alt="" src={require("../images/ham1.jpg")} ></img>
+          {/* <img alt="" src={require("../images/ham1.jpg")} ></img> */}
         </div>
       </div>
         
