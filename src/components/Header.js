a68285ecbe9ae237b02b49dc9e0f94f97bcecdf5
simona trotti <simona.trotti90@gmail.com>
2019-04-03 23:33:23 +0200

    logo inserted

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import '../style/css/header.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStarOfLife } from '@fortawesome/free-solid-svg-icons'
-library.add(faStarOfLife)
-
+import logo from '../images/logo.jpg';
 
 //token d4itqdmz67bhsnpymefac5jaalrygikz4fhhsmkp2p4hm7vj3b7q
 class Header extends Component {
   render() {
+   // var isActive = this.context.router.route.location.pathname === this.props.to;
+    var classActive =  'active' ;
     return (
       <header>
         <div className="logo">
-          <FontAwesomeIcon icon="star-of-life" />
-          <h1>sitoesempio</h1>
-          <h2>since 1994</h2>
+        <img  src={logo}  alt="logo"/>
         </div>
           <nav>
               <ul>
-                  <li><Link to='/'>Home</Link></li>
-                  <li><Link to='/about'>About</Link></li>
-                  <li><Link to='/contacts'>Contacts</Link></li>
+                  <li><NavLink activeClassName={classActive} exact={true} to='/'>Home</NavLink></li>
+                  <li><NavLink activeClassName={classActive} to='/about'>About</NavLink></li>
+                  <li><NavLink activeClassName={classActive} to='/contacts'>Contacts</NavLink></li>
               </ul>
           </nav>
       </header>
