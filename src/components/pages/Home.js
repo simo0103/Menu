bede6d487ccb37ba3bd922406c02562f9f072768
Simona Trotti <simona.trotti.external@ynap.com>
2019-04-05 17:25:22 +0200

    fix router

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import Header from '../Header';
-import MenuMain from '../menuPages/MenuMain';
+import Starters from '../menuPages/Starters';
+import Hamburger from '../menuPages/Hamburger';
+import Pizza from '../menuPages/Pizza';
+import Dessert from '../menuPages/Dessert';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
-    return (
-      <div className="Home">
-      <Header></Header> 
-      <MenuMain></MenuMain>
+    return ( 
+      <Router>
+       <div className="Home">
+        <ul>
+          <li>
+            <Link to="/">starters</Link>
+          </li>
+          <li>
+            <Link to="/hamburger">hambugers</Link>
+          </li>
+          <li>
+            <Link to="/pizza">pizza</Link>
+          </li>
+          <li>
+            <Link to="/dessert">dessert</Link>
+          </li>
+        </ul>
+        <Route exact path='/' component= {Starters}></Route>
+        <Route exact path='/hamburger' component= {Hamburger}></Route>
+        <Route exact path='/pizza' component= {Pizza}></Route>
+        <Route exact path='/dessert' component= {Dessert}></Route>
+        </div>
+      </Router>
 
-      
-
-      </div>
+     
         
     );
   }
