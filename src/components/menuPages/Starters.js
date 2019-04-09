50393012a74262ddc1648e6f8505f1ddef8be053
Simona Trotti <simona.trotti.external@ynap.com>
2019-04-09 16:57:32 +0200

    router in slider

--- src/components/menuPages/Starters.js
+++ src/components/menuPages/Starters.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import Api from '../../api/api';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Link, Switch } from "react-router-dom";
 import Slider from "react-slick";
 import '../../style/css/slider.css';
 
-
-
 //https://css-tricks.com/understanding-react-setstate/
+
 class Starters extends Component {
 
   render() {
@@ -20,17 +19,15 @@ class Starters extends Component {
     };
     return (
       <div className="starters">
-       <Slider {...settings}>
-       {
-           Api.allStarters().map((p, key) => (
-        <div className="slider-block">
-          <div><img key={key} alt={p.name} src={p.photo}></img></div>
-        </div>
-           ))
-       }
-       
-        </Slider>
-          {/* <Link to='/home'>Back</Link> */}
+     
+      <Slider {...settings}>
+        {
+          Api.allStarters().map(p => (            
+            <NavLink to={p.name} activeClassName="selected" exact={true}><img alt={p.name} src={p.photo}></img></NavLink>       
+          ))
+        }
+      </Slider>
+      
       </div>
     );
   }
