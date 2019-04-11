8c0d31206261dd4754c2fe444890ac658218c0e3
simona trotti <simona.trotti90@gmail.com>
2019-04-11 22:11:03 +0200

    cerco di collegare 2 component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
 import Main from './pages/Main';
+import Info from './pages/Info';
 // const App = () => (
 //   <div>
 //     <Header />
 //     <Main />
 //   </div>
 // )
+
+
 class App extends Component {
+  constructor() {
+    this.state = {
+      data : ""
+    }
+  }
+
+  onChangeLinkName(newName) {
+    this.setState({
+      data : newName
+    })
+  }
   render() {
     return (
       <div className="App">
         <NavBar />
-        <Main />
+        <Main changeLink = {this.onChangeLinkName.bind(this)} />
+        <Info data= {this.state.data}/>
       </div>
     );
   }
---
+++ src/components/pages/Info.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar';
+import Main from './pages/Main';
+// const App = () => (
+//   <div>
+//     <Header />
+//     <Main />
+//   </div>
+// )
+
+
+class Info extends Component {
+  constructor() {
+    
+  }
+
+ 
+  render() {
+    return (
+      <div className="Info">
+        
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Info;
